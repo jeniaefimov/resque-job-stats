939eac3ec66dd333c7a6f15a80f900c327b6ed9e
Eugene Efimov <eugene.efimov@sparksolutions.co>
2020-11-23 12:52:57 +0100

    Combine history with duration

--- lib/resque/plugins/job_stats/duration.rb
+++ lib/resque/plugins/job_stats/duration.rb
@@ -2,30 +2,52 @@ module Resque
   module Plugins
     module JobStats
       module Duration
-
-        # Resets all job durations
         def reset_job_durations
           Resque.redis.del(jobs_duration_key)
         end
 
-        # Returns the number of jobs failed
         def job_durations
           Resque.redis.lrange(jobs_duration_key,0,durations_recorded - 1).map(&:to_f)
         end
 
-        # Returns the key used for tracking job durations
+        def job_histories(start=0, limit=histories_recordable)
+          Resque.redis.lrange(jobs_history_key, start, start + limit - 1).map { |h| decode(h) }
+        end
+
+        def jobs_history_key
+          "stats:jobs:#{self.name}:history"
+        end
+
         def jobs_duration_key
           "stats:jobs:#{self.name}:duration"
         end
 
-        # Increments the failed count when job is complete
         def around_perform_job_stats_duration(*args)
           start = Time.now
-          yield
-          duration = Time.now - start
+          begin
+            yield
+            duration = Time.now - start
+            Resque.redis.lpush(jobs_duration_key, duration)
+            Resque.redis.ltrim(jobs_duration_key, 0, durations_recorded)
+            push_history "success" => true, "args" => args, "run_at" => start, "duration" => duration
+          rescue StandardError => e
+            duration = Time.now - start
+            exception = { "name" => e.to_s, "backtrace" => e.backtrace }
+            push_history "success" => false, "exception" => exception, "args" => args, "run_at" => start, "duration" => duration
+            raise e
+          end
+        end
 
-          Resque.redis.lpush(jobs_duration_key, duration)
-          Resque.redis.ltrim(jobs_duration_key, 0, durations_recorded)
+        def histories_recordable
+          @histories_recordable || 100
+        end
+
+        def histories_recorded
+          Resque.redis.llen(jobs_history_key)
+        end
+
+        def reset_job_histories
+          Resque.redis.del(jobs_history_key)
         end
 
         def durations_recorded
@@ -42,6 +64,12 @@ module Resque
           job_durations.max.to_f
         end
 
+        private
+
+        def push_history(history)
+          Resque.redis.lpush(jobs_history_key, encode(history))
+          Resque.redis.ltrim(jobs_history_key, 0, histories_recordable)
+        end
       end
     end
   end
